32c1c82c47e4b3c1a38e58c8f6dd86cdbf83a6a9
ClaytonTaylor <claytaylor@cox.net>
2017-07-19 13:38:10 -0500

    Index.html

--- user.js
+++ user.js
@@ -1,5 +1,18 @@
+console.log("User.js is Loaded");
 
+(() => {
+  var parent = document.querySelector('body');
+  var display = document.createElement('span');
+  var input = document.createElement('input');
+  var button = document.createElement('button');
 
-function customUrl(userInput) {
-return `http://google.com/${userInput}/customer`;
-}
+button.addEventListener('click', () => {
+
+display.innerText = `http://google.com/${input.value}/customer`;
+
+});
+
+parent.appendChild(input);
+parent.appendChild(button);
+parent.appendChild(display);
+})();
